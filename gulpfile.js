de0a700dabe20f24a9939c642977fda8a43353cc
Christy Haragan <christy@christy.haragan@gmail.com>
2015-06-17 16:14:50 +0100

    Fix name to consistent uservices

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var dts = require('dts-generator');
 
 gulp.task('default', function(){
   dts  .generate({
-    name: 'uservice',
+    name: 'uservices',
     baseDir: './module',
     excludes: ['./typings/node/node.d.ts', './typings/typescript/lib.es6.d.ts'],
     files: [ './index.ts' ],
-    out: './uservice.d.ts'
+    out: './uservices.d.ts'
   });
 })
