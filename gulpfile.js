6dd5624e942d944cc3d53481556e13dd1133c162
Christy Haragan <christy.haragan@gmail.com>
2015-06-17 21:15:11 +0100

    Remove unnecessary line in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ gulp.task('default', function(){
   dts  .generate({
     name: 'uservices',
     baseDir: './lib',
-    excludes: ['./typings/node/node.d.ts', './typings/es6/es6.d.ts'],
     files: [ './index.ts' ],
     out: './uservices.d.ts',
     main: 'index'
