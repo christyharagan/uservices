c701a2e77b88332ce7867ef02b646847828fb4ba
Christy Haragan <christy.haragan.github@gmail.com>
2015-10-27 16:25:53 +0000

    Add jsonify and dejsonify methods

--- lib/serialiserParser.d.ts
+++ lib/serialiserParser.d.ts
@@ -1,3 +1,5 @@
 import * as m from './model';
-export declare function stringify(service: m.Services<any>): string;
+export declare function stringify(services: m.Services<any>): string;
+export declare function jsonify(services: m.Services<any>): any;
 export declare function parse(serviceString: string): m.Services<any>;
+export declare function dejsonify(jsonified: any): m.Services<any>;
--- src/lib/serialiserParser.ts
+++ src/lib/serialiserParser.ts
@@ -14,46 +14,91 @@ function stringifyReplacer(key: string, value: any): any {
   }
 }
 
-export function stringify(service: m.Services<any>): string {
-  return JSON.stringify(service, stringifyReplacer, '  ')
+export function stringify(services: m.Services<any>): string {
+  return JSON.stringify(jsonify(services), null, '  ')
 }
 
-export function parse(serviceString: string): m.Services<any> {
-  let services = <m.Services<any>>JSON.parse(serviceString)
-
+export function jsonify(services: m.Services<any>): any {
+  let jsonified = {}
   Object.keys(services).forEach(function(name) {
     let service = services[name]
-    service.name = name
+    let jsonifiedService = {
+      methods: {},
+      events: {}
+    }
+    jsonified[name] = jsonifiedService
 
     if (service.methods) {
       Object.keys(service.methods).forEach(function(name) {
         let method = service.methods[name]
-        method.name = name
-        method.service = service
+        let jsonifiedMethod = {}
+        Object.keys(method).forEach(function(key){
+          if (key !== 'name' && key !== 'service' && key !== 'type') {
+            jsonifiedMethod[key] = method[key]
+          }
+        })
 
-        if (method.type) {
-          // TODO
-        }
+        jsonifiedService.methods[name] = jsonifiedMethod
       })
-    } else {
-      service.methods = {}
     }
     if (service.events) {
       Object.keys(service.events).forEach(function(name) {
         let event = service.events[name]
-        event.name = name
-        event.service = service
-
-        if (event.type) {
-          // TODO
-        }
+        let jsonifiedEvent = {}
+        Object.keys(event).forEach(function(key){
+          if (key !== 'name' && key !== 'service' && key !== 'type') {
+            jsonifiedEvent[key] = event[key]
+          }
+        })
+        jsonifiedService.events[name] = jsonifiedEvent
       })
-    } else {
-      service.events = {}
     }
+  })
+  return jsonified
+}
 
-    if (service.type) {
-      // TODO
+export function parse(serviceString: string): m.Services<any> {
+  return dejsonify(JSON.parse(serviceString))
+}
+
+export function dejsonify(jsonified:any): m.Services<any> {
+  let services:m.Services<any> = {
+  }
+
+  Object.keys(jsonified).forEach(function(name) {
+    let jsonifiedService = jsonified[name]
+    let service:m.Service<any, any> = {
+      name: name,
+      methods: {},
+      events: {}
+    }
+    services[name] = service
+
+    if (jsonifiedService.methods) {
+      Object.keys(jsonifiedService.methods).forEach(function(name) {
+        let jsonifiedMethod = jsonifiedService.methods[name]
+        let method:m.Method<any> = {
+          name: name,
+          service: service
+        }
+        Object.keys(jsonifiedMethod).forEach(function(key){
+          method[key] = jsonifiedMethod[key]
+        })
+        service.methods[name] = method
+      })
+    }
+    if (jsonifiedService.events) {
+      Object.keys(jsonifiedService.events).forEach(function(name) {
+        let jsonifiedEvent = jsonifiedService.events[name]
+        let event = {
+          name: name,
+          service: service
+        }
+        Object.keys(jsonifiedEvent).forEach(function(key){
+          event[key] = jsonifiedEvent[key]
+        })
+        service.events[name] = event
+      })
     }
   })
 
